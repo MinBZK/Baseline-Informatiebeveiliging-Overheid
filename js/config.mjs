a8f93da41d6610fe16c5bc9ca96ee0ca02ba7440
BartL-CIP <bart.leegwater@cip-overheid.nl>
2026-05-20 11:51:12 +0200

    tijdelijk3

--- js/config.mjs
+++ js/config.mjs
@@ -1,4 +1,4 @@
-import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+import { loadRespecWithConfiguration } from "./organisation-config.mjs";
 
 loadRespecWithConfiguration({
   useLogo: true,
