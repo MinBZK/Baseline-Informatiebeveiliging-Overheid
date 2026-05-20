505c17f52342307d1b69c68c076fbc4c228cd862
BartL-CIP <bart.leegwater@cip-overheid.nl>
2026-05-20 11:18:13 +0200

    tijdelijk

---
+++ js/config.mjs
@@ -0,0 +1,35 @@
+import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+
+loadRespecWithConfiguration({
+  useLogo: true,
+  useLabel: true,
+  license: "cc-by",
+  specStatus: "WV",
+  specType: "HR",
+  pubDomain: "dk",
+  shortName: "template",
+  publishDate: "2023-06-21",
+  publishVersion: "0.0.3",
+
+  // TODO: Verwijder voordat de release plaats vindt
+  latestVersion: "https://github.com/logius-standaarden/respec-template/",
+  prevVersion: [],
+
+  editors:
+    [
+      {
+        name: "Naam van persoon hier",
+        company: "Logius",
+        companyURL: "https://www.logius.nl",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Naam van persoon hier",
+        company: "Logius",
+        companyURL: "https://www.logius.nl",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/ReSpec-template",
+});
